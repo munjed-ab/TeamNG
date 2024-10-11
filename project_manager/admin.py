82103d5dd0076d716c2ad01eed22c62b84009181
munjed-ab <abazamunged@gmail.com>
2024-10-11 19:57:07 +0300

    Added list_display to Leave

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from .models import CustomUser, Project, Activity, Holiday, Leave, Notification, ActivityLogs, Department, Location, Profile, Role
 # Register your models here.
 
+class LeaveAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Leave._meta.fields if field.name != "id"]
+
 admin.site.register(CustomUser)
 admin.site.register(Project)
 admin.site.register(Activity)
 admin.site.register(Holiday)
-admin.site.register(Leave)
+admin.site.register(Leave, LeaveAdmin)
 admin.site.register(ActivityLogs)
 admin.site.register(Notification)
 admin.site.register(Department)
